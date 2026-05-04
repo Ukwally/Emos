58e26ffea5a3416781a0bb560c618780983559de
ukwally <ukwally22rgmail.com>
2026-05-04 17:37:57 +0100

    Melhorar a interface

---
+++ assets/scripts/index.js
@@ -0,0 +1,29 @@
+document.addEventListener('scroll', () => {
+    const btnTopo = document.getElementById('btn-topo');
+    window.scrollY > 100 ? btnTopo.classList.add('visivel') : btnTopo.classList.remove('visivel');
+
+
+
+    const header = document.getElementById('header');
+    const head1 = document.getElementById('head1');
+    const head2 = document.getElementById('head2');
+    const head3 = document.getElementById('head3');
+
+    if (window.scrollY > 100) {
+        header.classList.add('visivel');
+        head1.style = 'display:none';
+    } else {
+        header.classList.remove('visivel');
+        head1.style = 'display:flex';
+    }
+
+    //const tela = document.getElementById('body').getBoundingClientRect().bottom;
+    //Ocalculo abaixo precisa ser revisto;
+    const tela = document.body.scrollHeight;
+    const fimDatela = tela - 900;
+    if (window.scrollY > fimDatela) {
+        btnTopo.classList.add('opaco')
+    } else {
+        btnTopo.classList.remove('opaco')
+    }
+});
