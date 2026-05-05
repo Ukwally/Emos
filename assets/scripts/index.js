3d59208804baf9f674d6222bf076b9aa3be9ddc5
ukwally <ukwally22rgmail.com>
2026-05-05 16:05:48 +0100

    Começar as animações

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,5 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('banner-img-set').classList.add('ativo');
+    document.getElementById('banner-txt-small').classList.add('ativo');
+    document.getElementById('banner-txt-h').classList.add('ativo');
 })
 document.addEventListener('scroll', () => {
     //Fixar header abaixo
